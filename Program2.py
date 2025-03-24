72864c4a54da1456443e6d32b8783ce2a0b0f381
vynkypy <57390573+vynkypy@users.noreply.github.com>
2025-03-24 21:10:33 +0530

    Add files via upload
    addition of two numbers

---
+++ Program2.py
@@ -0,0 +1,9 @@
+# addition of two numbers
+
+num_a = int(input('Enter first number: '))
+
+num_b = int(input('Enter second number: '))
+
+ans = num_a + num_b
+
+print('Addition of two numbers is: ', ans)
