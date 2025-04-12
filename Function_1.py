97092a30e58b3320e069cef878ea82653bac9028
vynkypy <57390573+vynkypy@users.noreply.github.com>
2025-04-13 00:01:46 +0530

    function that calculates the factorial of a number

---
+++ Function_1.py
@@ -0,0 +1,13 @@
+# 1️⃣ Define a function that calculates the factorial of a number.
+
+
+def factorial(var1):
+    temp = 1
+
+    for i in range(1, var1+1):
+        temp = i*temp
+    return temp
+
+
+num = int(input("Enter a number: "))
+print("The factorial of given number is: ", factorial(num))
