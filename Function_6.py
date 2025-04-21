2d248481286f6bd67f18cfaa4c701f202709c38a
vynkypy <57390573+vynkypy@users.noreply.github.com>
2025-04-21 13:31:42 +0530

    Square of a number

---
+++ Function_6.py
@@ -0,0 +1,12 @@
+# 6 Write a function that finds sequre of number.
+
+num = float(input("Enter the number for which you want squre: "))
+
+
+def cel_to_fah(no):
+    return no**2
+
+
+result = cel_to_fah(num)
+
+print("Squer of given number is:", result)
