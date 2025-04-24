d0140c820ab16b24afcb94b4e85580bd5149a885
vynkypy <57390573+vynkypy@users.noreply.github.com>
2025-04-24 10:22:08 +0530

    Accept single digit number and print it into word

---
+++ conditionL.py
@@ -0,0 +1,33 @@
+# Accept single digit number from user and print it into word.
+
+num = int(input("Enter a single digit: "))
+
+if num >= 10:
+    print("Invalid Number")
+
+elif num == 1:
+    print("One")
+
+elif num == 2:
+    print("Two")
+
+elif num == 3:
+    print("Three")
+
+elif num == 4:
+    print("Four")
+
+elif num == 5:
+    print("Five")
+
+elif num == 6:
+    print("Six")
+
+elif num == 7:
+    print("Seven")
+
+elif num == 8:
+    print("Eight")
+
+elif num == 9:
+    print("Nine")
